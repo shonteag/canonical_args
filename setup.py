74c7473e54c49d4b93e49888e74b80220cc292ba
shonteag <shonte.amatogrill@gmail.com>
2018-04-12 11:15:43 -0400

    + setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+
+
+setup(name="canonical_args",
+	  version="0.1",
+	  author="Shonte Amato-Grill",
+	  author_email="shonte.amatogrill@gmail.com",
+	  maintainer="Shonte Amato-Grill",
+	  maintainer_email="shonte.amatogrill@gmail.com",
+	  packages=find_packages(exclude=["test", "tests"])
+)
